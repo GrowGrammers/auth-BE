956d7010deeadeaa5d79387a40e8aff19c166de3
HJunng <guswjd1gh@gmail.com>
2025-08-11 12:19:08 +0900

    refactor: Responses 분리 및 BaseResponse 정리
    * `Responses`를 별도 파일로 분리하여 코드 가독성 향상
    * `BaseResponse`에 불필요한 `Responses` 관련 코드 제거

---
+++ src/main/kotlin/com/wq/demo/web/common/BaseResponse.kt
@@ -0,0 +1,8 @@
+package com.wq.demo.web.common
+
+data class BaseResponse<T> (
+    val success: Boolean,
+    val message: String,
+    val data: T?,
+    val error: String? = null
+)
--- src/main/kotlin/com/wq/demo/web/common/Responses.kt
+++ src/main/kotlin/com/wq/demo/web/common/Responses.kt
@@ -2,13 +2,6 @@ package com.wq.demo.web.common
 
 import com.wq.demo.shared.error.ApiCode
 
-data class BaseResponse<T> (
-    val success: Boolean,
-    val message: String,
-    val data: T?,
-    val error: String? = null
-)
-
 object Responses {
     fun <T> success(
         message: String = "요청에 성공적으로 응답하였습니다.",
