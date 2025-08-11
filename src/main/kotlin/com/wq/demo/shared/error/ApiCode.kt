cc33523ebad68f26702457c5f792c4e897e526c2
HJunng <guswjd1gh@gmail.com>
2025-08-11 12:11:44 +0900

    feat: ApiException, ApiCode 기본 정의
    * ApiCode: Http Status와 메세지 반환하는 인터페이스
    * ApiException: ApiCode를 포함한 표준 예외 클래스
    * extractExceptionLocation(): 발생 위치를 간결하게 추출 -> log용.

--- src/main/kotlin/com/wq/demo/shared/error/ApiCode.kt
+++ src/main/kotlin/com/wq/demo/shared/error/ApiCode.kt
@@ -2,5 +2,5 @@ package com.wq.demo.shared.error
 
 interface ApiCode {
     fun getStatus(): Int // HTTP status code (e.g., 200, 404, 401)
-    fun getMessage(): String? // 사용자 표시용 메시지
+    fun getMessage(): String // 사용자 표시용 메시지
 }
