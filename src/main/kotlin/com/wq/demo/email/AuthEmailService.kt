ba7944fe5aed2e37f9b90d24e476b357ab85b617
Mint <jrkd2165@gmail.com>
2025-08-12 17:59:49 +0900

    feat: 이메일 인증 코드 전송 구현

---
+++ src/main/kotlin/com/wq/demo/email/AuthEmailService.kt
@@ -0,0 +1,26 @@
+package com.wq.demo.email
+
+import com.wq.demo.shared.utils.VerificationCodeGenerator.generateRandomCode
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.stereotype.Service
+
+@Service
+class AuthEmailService (
+    private val emailRepository: AuthEmailRepository,
+    private val mailSender: JavaMailSender,
+) {
+    fun sendVerificationCode(email: String) {
+        val code = generateRandomCode();
+        emailRepository.save(EmailVerification(email, code))
+        sendEmail(email, "인증 코드", "인증 코드는 $code 입니다.")
+    }
+
+    fun sendEmail(to: String, subject: String, text: String) {
+        val message = SimpleMailMessage()
+        message.setTo(to)
+        message.setSubject(subject)
+        message.setText(text)
+        mailSender.send(message)
+    }
+}
