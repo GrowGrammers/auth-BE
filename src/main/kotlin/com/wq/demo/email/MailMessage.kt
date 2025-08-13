49fd1d3e573f5594f98b7cf6f4d006119348a908
Mint <jrkd2165@gmail.com>
2025-08-13 19:56:39 +0900

    refactor: 메일 객체 변경

--- src/main/kotlin/com/wq/demo/email/MailMessage.kt
+++ src/main/kotlin/com/wq/demo/email/MailMessage.kt
@@ -2,18 +2,13 @@ package com.wq.demo.email
 
 import org.springframework.mail.SimpleMailMessage
 
-class MailMessage private constructor(
-    val to: String,
-    val subject: String,
-    val text: String
-) {
+class MailMessage private constructor() {
     companion object {
-        fun of(to: String, subject: String, text: String): SimpleMailMessage {
-            return SimpleMailMessage().apply {
+        fun of(to: String, subject: String, text: String): SimpleMailMessage =
+            SimpleMailMessage().apply {
                 setTo(to)
                 setSubject(subject)
                 setText(text)
             }
-        }
     }
 }
