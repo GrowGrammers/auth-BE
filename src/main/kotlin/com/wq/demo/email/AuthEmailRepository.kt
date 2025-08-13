f8f8c1d0c45823bfbe21f583126651aec7fdf332
Mint <jrkd2165@gmail.com>
2025-08-13 17:50:41 +0900

    refactor: 이메일 레파지토리 어노테이션 제거

--- src/main/kotlin/com/wq/demo/email/AuthEmailRepository.kt
+++ src/main/kotlin/com/wq/demo/email/AuthEmailRepository.kt
@@ -3,7 +3,6 @@ package com.wq.demo.email
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-@Repository
 interface AuthEmailRepository : JpaRepository<EmailVerification, String> {
     fun findByEmail(email: String): EmailVerification?
 }
