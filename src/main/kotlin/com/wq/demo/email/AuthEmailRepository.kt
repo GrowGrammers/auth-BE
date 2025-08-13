05b2326b927733ce08d242b3bed25e16e2410690
Mint <jrkd2165@gmail.com>
2025-08-13 23:35:20 +0900

    style: entity 객체명 수정에 따른 레파지토리 수정

--- src/main/kotlin/com/wq/demo/email/AuthEmailRepository.kt
+++ src/main/kotlin/com/wq/demo/email/AuthEmailRepository.kt
@@ -2,6 +2,6 @@ package com.wq.demo.email
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface AuthEmailRepository : JpaRepository<EmailVerification, String> {
-    fun findByEmail(email: String): EmailVerification?
+interface AuthEmailRepository : JpaRepository<EmailVerificationEntity, String> {
+    fun findByEmail(email: String): EmailVerificationEntity?
 }
