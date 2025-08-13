be8b2abeb4d6ca8d9b519b86ce068d36131b54bf
Mint <jrkd2165@gmail.com>
2025-08-13 19:53:04 +0900

    style: 레파지토리 불필요한 import문 제거

--- src/main/kotlin/com/wq/demo/email/AuthEmailRepository.kt
+++ src/main/kotlin/com/wq/demo/email/AuthEmailRepository.kt
@@ -1,7 +1,6 @@
 package com.wq.demo.email
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
 interface AuthEmailRepository : JpaRepository<EmailVerification, String> {
     fun findByEmail(email: String): EmailVerification?
