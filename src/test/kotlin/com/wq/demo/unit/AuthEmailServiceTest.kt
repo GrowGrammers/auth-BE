e76861bcff4714cd961660aca7d9fc4b5cca5b7d
Mint <jrkd2165@gmail.com>
2025-08-19 17:14:21 +0900

    test: 이메일 인증 에러 추가에 따른 테스트코드 수정

--- src/test/kotlin/com/wq/demo/unit/AuthEmailServiceTest.kt
+++ src/test/kotlin/com/wq/demo/unit/AuthEmailServiceTest.kt
@@ -3,6 +3,8 @@ package com.wq.demo.unit
 import com.wq.demo.email.AuthEmailRepository
 import com.wq.demo.email.AuthEmailService
 import com.wq.demo.email.EmailVerificationEntity
+import com.wq.demo.email.error.EmailException
+import com.wq.demo.email.error.EmailExceptionCode
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.assertions.throwables.shouldThrow
@@ -22,21 +24,23 @@ class AuthEmailServiceTest : StringSpec({
         authEmailService.sendVerificationCode(validEmail)
     }
 
-    "잘못된 이메일 형식은 AddressException 발생" {
+    "잘못된 이메일 형식 INVALID_EMAIL_FORMAT Exception 발생" {
         val invalidEmail = "invalid-email"
-        shouldThrow<AddressException> {
+        val exception = shouldThrow<EmailException> {
             authEmailService.sendVerificationCode(invalidEmail)
         }
+        exception.emailCode shouldBe EmailExceptionCode.INVALID_EMAIL_FORMAT
     }
 
-    "존재하지 않는 도메인은 IllegalArgumentException 발생" {
+    "존재하지 않는 도메인 검증 시 DOMAIN_NOT_FOUND EmailException 발생" {
         val nonExistentDomainEmail = "user@no-such-domain.zzz"
-        shouldThrow<IllegalArgumentException> {
+        val exception = shouldThrow<EmailException> {
             authEmailService.sendVerificationCode(nonExistentDomainEmail)
         }
+        exception.emailCode shouldBe EmailExceptionCode.DOMAIN_NOT_FOUND
     }
 
-    "메일 전송 실패 시 IllegalStateException 발생" {
+    "메일 전송 실패 시 EMAIL_NOT_SENDED EmailException 발생" {
         val email = "test@gmail.com"
         val code = "123456"
 
@@ -45,12 +49,12 @@ class AuthEmailServiceTest : StringSpec({
             .whenever(mailSender)
             .send(any<SimpleMailMessage>())
 
-        shouldThrow<IllegalStateException> {
+        val exception = shouldThrow<EmailException> {
             authEmailService.sendEmail(email, "인증코드", "인증 코드는 $code 입니다.")
         }
+        exception.emailCode shouldBe EmailExceptionCode.EMAIL_NOT_SENDED
     }
-
-    "인증 코드 검증" {
+    "인증 코드 검증 성공" {
         val email = "user@gmail.com"
         val code = "123456"
         val entity = EmailVerificationEntity(email, code)
@@ -58,8 +62,21 @@ class AuthEmailServiceTest : StringSpec({
         // repository 동작 모킹
         whenever(emailRepository.findByEmail(email)).thenReturn(entity)
 
-        authEmailService.verifyCode(email, code) shouldBe true
-        authEmailService.verifyCode(email, "wrong-code") shouldBe false
+        authEmailService.verifyCode(email, code)
+    }
+
+    "잘못된 인증 코드 검증 시 EMAIL_VERIFICATION_FAILED EmailException 발생" {
+        val email = "user@gmail.com"
+        val code = "123456"
+        val entity = EmailVerificationEntity(email, code)
+
+        // repository 동작 모킹
+        whenever(emailRepository.findByEmail(email)).thenReturn(entity)
+
+        val exception = shouldThrow<EmailException> {
+            authEmailService.verifyCode(email, "wrong-code")
+        }
+        exception.emailCode shouldBe EmailExceptionCode.EMAIL_VERIFICATION_FAILED
     }
 
 })
