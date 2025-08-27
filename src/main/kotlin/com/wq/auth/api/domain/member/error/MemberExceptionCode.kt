a81b5667e8d393185b4e14a397c8317212b93948
Mint <jrkd2165@gmail.com>
2025-08-27 16:57:57 +0900

    feat: Member 도메인 커스텀 에러 추가

---
+++ src/main/kotlin/com/wq/auth/api/domain/member/error/MemberExceptionCode.kt
@@ -0,0 +1,10 @@
+package com.wq.auth.api.domain.member.error
+
+import com.wq.auth.shared.error.ApiResponseCode
+
+enum class MemberExceptionCode (
+    override val status: Int,
+    override val message: String
+) : ApiResponseCode {
+    DATABASE_SAVE_FAILED(500, "회원 정보를 저장하는데 실패했습니다.")
+}
