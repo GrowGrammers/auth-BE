a6bf6df8878f7406df0bb76b250598ce93b6c327
Mint <jrkd2165@gmail.com>
2025-10-01 15:07:34 +0900

    Refactor/WQ-165 email verification login (#43)
    * feat: 이메일 인증 5분 제한 추가
    
    * fix: 이메일 인증 로직 최신 인증 번호만 사용하도록 수정
    
    * style: 불필요한 주석 제거
    
    * test: 이메일 인증 로직 수정에 따른 테스트 코드 수정
    
    * fix: 사용자 검색 로직 수정 (이메일 -> 이메일 + providerType)
    
    * test: 사용자 검색 로직 수정에 따른 테스트코드 수정

--- src/main/kotlin/com/wq/auth/api/domain/email/AuthEmailService.kt
+++ src/main/kotlin/com/wq/auth/api/domain/email/AuthEmailService.kt
@@ -8,6 +8,7 @@ import jakarta.mail.internet.InternetAddress
 import org.slf4j.LoggerFactory
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
+import java.time.Instant
 import java.util.*
 import javax.naming.directory.InitialDirContext
 
@@ -26,7 +27,6 @@ class AuthEmailService(
         val code = generateRandomCode()
         log.debug("Generated verification code={} for email={}", code, email)
 
-        //TODO 인증번호 5분 제한, 제거시 soft delete
         sendEmail(email, "인증 코드", "인증 코드는 $code 입니다.")
         emailRepository.save(EmailVerificationEntity(email, code))
         log.info("Verification code saved for email={}", email)
@@ -34,13 +34,29 @@ class AuthEmailService(
 
     fun verifyCode(email: String, code: String) {
         log.info("verifyCode() called for email={} with code={}", email, code)
-        val savedCode = emailRepository.findByEmail(email)?.code
+
+        //DB에 x
+        val emailVerificationEntity = emailRepository.findFirstByEmailOrderByCreatedAtDesc(email)
+            ?: throw EmailException(EmailExceptionCode.EMAIL_VERIFICATION_FAILED)
+
+        val savedCode = emailVerificationEntity.code
+        val createdAt = emailVerificationEntity.createdAt
+
         log.debug("Saved code from DB={} for email={}", savedCode, email)
 
-        if (savedCode == null || savedCode != code) {
-            log.warn("Email verification failed for email={}", email)
+        // 5분 제한 체크
+        val expired = createdAt.plusSeconds(300).isBefore(Instant.now())
+        if (expired) {
+            log.warn("Verification code expired for email={}", email)
+            throw EmailException(EmailExceptionCode.EMAIL_EXPIRED)
+        }
+
+        //코드 불일치
+        if (savedCode != code) {
+            log.warn("Invalid verification code for email={}", email)
             throw EmailException(EmailExceptionCode.EMAIL_VERIFICATION_FAILED)
         }
+
         log.info("Email verification success for email={}", email)
     }
 
--- src/main/kotlin/com/wq/auth/api/domain/email/entity/EmailVerificationEntity.kt
+++ src/main/kotlin/com/wq/auth/api/domain/email/entity/EmailVerificationEntity.kt
@@ -1,13 +1,23 @@
 package com.wq.auth.api.domain.email.entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import org.springframework.data.annotation.CreatedDate
+import java.time.Instant
 
 @Entity
 @Table(name = "email_verification")
 data class EmailVerificationEntity(
     @Id
+    @Column(nullable = false, unique = true, length = 255)
     val email: String,
-    val code: String
+
+    @Column(nullable = false, length = 10)
+    val code: String,
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    val createdAt: Instant = Instant.now()
 )
